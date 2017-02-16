a568f9aa7d532f8432c5d4ca98b1eadbdf221ca9
wangzs <wangzs@smartdot.com>
2017-02-16 15:55:53 +0800

    小型优化

--- category-listings/JPCommentHeaderView.m
+++ category-listings/JPCommentHeaderView.m
@@ -17,10 +17,6 @@
         
         self.contentView.backgroundColor = [UIColor whiteColor];
         
-        UIButton * groupBtn=[[UIButton alloc]initWithFrame:CGRectMake(0, 5, SCREEN_WIDTH, 60)];
-        [groupBtn addTarget:self action:@selector(btnClick) forControlEvents:UIControlEventTouchUpInside];
-        [self.contentView addSubview:groupBtn];
-        
         _groupLeftV=[[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 40, 40)];
         _groupLeftV.center=CGPointMake(35, 35);
         [self.contentView addSubview:_groupLeftV];
@@ -32,24 +28,34 @@
         
         _groupImageV=[[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 13, 8)];
         _groupImageV.image=[UIImage imageNamed:@"catalogs_arrow"];
-        _groupImageV.center=CGPointMake(SCREEN_WIDTH-50, 35);
+        _groupImageV.center=CGPointMake(SCREEN_WIDTH-20, 35);
         [self.contentView addSubview:_groupImageV];
         
         UIImageView *lineImageV=[[UIImageView alloc]initWithFrame:CGRectMake(0, 69.5, SCREEN_WIDTH, 0.5)];
         lineImageV.backgroundColor=[UIColor colorWithWhite:0.95 alpha:1];
         [self.contentView addSubview:lineImageV];
+        
+        UITapGestureRecognizer *tapGR = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tapGR:)];
+        [self.contentView addGestureRecognizer:tapGR];
     }
     return self;
 }
 
-- (void)btnClick{
-    
-    [UIView animateWithDuration:0.3 delay:0.0 options:UIViewAnimationOptionTransitionNone animations:^{
+-(void)tapGR:(UITapGestureRecognizer *)tapGR{
+    [UIView animateWithDuration:0.3 animations:^{
         CGAffineTransform currentTransform = _groupImageV.transform;
         // 在现在的基础上旋转指定角度
         CGAffineTransform newTransform = CGAffineTransformRotate(currentTransform, -M_PI);
         _groupImageV.transform = newTransform;
-    }completion:nil];
+
+    }];
+
+//    [UIView animateWithDuration:0.3 delay:0.0 options:UIViewAnimationOptionTransitionNone animations:^{
+//        CGAffineTransform currentTransform = _groupImageV.transform;
+//        // 在现在的基础上旋转指定角度
+//        CGAffineTransform newTransform = CGAffineTransformRotate(currentTransform, -M_PI);
+//        _groupImageV.transform = newTransform;
+//    }completion:nil];
     
     if ([self.delegate respondsToSelector:@selector(AddOrDelete:)]){
         [self.delegate AddOrDelete:self.tag];
